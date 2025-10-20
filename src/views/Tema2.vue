ca4e6da7cae84b12f0c4550fa475d0bb70e6f042
LISETHCP <llcelisp@gmail.com>
2025-10-20 12:51:16 -0500

    A4

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -81,20 +81,102 @@
                 p.mb-4.mb-lg-5.mb-md-5 Corresponde a la relación de equilibrio o simetría existente entre las partes de un todo. Este concepto resulta esencial en disciplinas como el arte, la moda o la arquitectura.
 
 
+    .row.justify-content-center.align-items-center.mb-4(data-aos="fade-left")
+      .col-auto.ps-lg-3.pe-lg-0.ps-0.pe-0
+        .subti
+      .col.ps-lg-0
+        .ssu.p-lg-2.p-3.pe-lg-4.ps-lg-4.pe-md-4.ps-md-4.pe-4.ps-4
+          h4.mb-0 Importancia de la proporción en el diseño
 
 
+    .row.justify-content-center.align-items-center.mb-2
+      .col-lg-9
+        .ap6.p-lg-5.p-4.mb-4.mb-lg-0
+          .row.justify-content-center.align-items-center.mb-0.my-lg-3.mb-lg-3.my-0
+            .col-lg-9
+            p.mb-0 El concepto y aplicación de la proporcionalidad en el diseño es fundamental para lograr un equilibrio armonioso dentro de las composiciones de una prenda de vestir, dado que relacionan las diferentes partes de un modelo entre sí y con el cuerpo humano.
+      .col-lg-3
+        img.d-none.d-lg-block(src='@/assets/curso/tema2/6.svg', alt='Imagen decorativa')
 
 
+    p.mb-4 A continuación, se presenta el ejemplo de la figura humana masculina en canon y proporción:
 
+
+    .row.justify-content-center.align-items-center.mb-0(data-aos="fade-right")
+      .col-lg-8
+        .titulo-sexto.mb-4
+          h5 Figura 4.
+          | Canon infantil por años
+        .ap3.mb-2.p-4
+          img.img-t.img-a(src='@/assets/curso/tema2/7.svg', alt='Cuatro figuras de un modelo anatómico infantil en escala de grises. A la izquierda, el cuerpo está dividido en proporciones basadas en la medida de la cabeza.')
+        
 
 
     separador
     #t_2_3.titulo-segundo.mb-5
       h2 2.3 Proporciones corporales
 
+
+    .ap7.mb-5
+        .row.justify-content-center.align-items-center
+          .col-lg-5.col-md-8.col-12.mb-lg-0.mb-3(data-aos="fade-right"): img(src='@/assets/curso/tema2/8.png', alt='')
+          .col-lg-7(data-aos="fade-left")
+            .p-4
+              p.mb-4(data-aos="fade-left") La proporción corporal alude a la relación matemática y armoniosa entre las distintas partes del cuerpo humano (Esparza, 1993). Esta relación se mantiene a lo largo de todas las etapas del desarrollo humano, desde el nacimiento hasta la vejez, y constituye la base para lograr patrones equilibrados y prendas con un ajuste adecuado.
+              p.mb-0(data-aos="fade-left") En el campo del patronaje y la confección, el estudio de las proporciones no solo permite obtener un equilibrio estético, sino también garantizar la funcionalidad, comodidad y ergonomía de las prendas. Dichas proporciones se convierten en un referente técnico para diseñadores, modelistas y confeccionistas.
+      
+
+    .row.justify-content-center.align-items-center.mb-0(data-aos="fade-right")
+      .col-lg-8
+        .titulo-sexto.mb-4
+          h5 Figura 5.
+          | Proporción de acuerdo con las etapas de crecimiento
+        .ap3.mb-2.p-4
+          img.img-t.img-a(src='@/assets/curso/tema2/9.png', alt='Serie de figuras humanas que representan el crecimiento desde recién nacido hasta adultez, comparando las proporciones corporales mediante el número de cabezas utilizadas como unidad de medida: 4 en recién nacidos, 5 a los 2 años, 6 a los 6 años, 7 a los 12 años y 8 a los 25 años.')
+        figcaption.mb-5.enl Fuente: Tomado de https://www.comocubriruncuerpo.org/proporciones-del-cuerpo-humano-1-da-vinci-y-vitruvio/
+
+
+
+
     separador
     #t_2_4.titulo-segundo.mb-5
       h2 2.4 Medidas deducidas
+
+
+    .cajon-b.color-primario.p-3.mb-5
+      p.mb-0 Las medidas son unidades numéricas empleadas para representar el tamaño de una figura, objeto o parte específica; en el patronaje de prendas de vestir, estos datos se obtienen utilizando la cinta métrica de manera lineal, con el fin de registrar el largo, el ancho y el contorno de las diferentes partes del cuerpo. 
+
+
+
+    .row.fondo1.justify-content-center.align-items-center.p-5.mb-0(data-aos="fade-down")
+      .col-lg-12.px-lg-5.px-4.mb-0
+        
+        .ap7.mt-4.mb-4
+          .row.justify-content-center.align-items-center
+            .col-lg-8(data-aos="fade-left")
+              .p-4
+                .row.justify-content-center.align-items-center.mb-0.mx-4
+                  .col-lg-2
+                    img(src="@/assets/curso/tema2/11.svg")
+                  .col-lg-10  
+                    p.mb-0.mx-lg-4(data-aos="fade-left") En Colombia, la metodología más común consiste en expresarlas en centímetros o pulgadas.
+            .col-lg-4.col-md-8.col-12.mb-lg-0.mb-0(data-aos="fade-right"): img(src='@/assets/curso/tema2/10.png', alt='')
+
+
+    .bloque-texto-g.bloque-texto-g--inverso.color-primario3.p-4.p-md-4.p-lg-5.mb-5(data-aos="fade-left")
+      .bloque-texto-g__img(
+        :style="{'background-image': `url(${require('@/assets/curso/tema2/12.png')})`}"
+      )
+      .bloque-texto-g__texto.p-lg-5.p-4
+        p.mb-0 Incluyen materiales de uso médico o quirúrgico que, aunque no poseen acción farmacológica directa, son fundamentales para la atención en salud. Dentro de esta categoría se encuentran elementos como jeringas, guantes, gasas, catéteres, soluciones intravenosas y cintas adhesivas.
+        br
+        br
+        p.mb-0 Debido a su alta rotación y demanda constante, especialmente en instituciones hospitalarias y servicios de urgencias, es indispensable que los proveedores aseguren disponibilidad permanente, entregas oportunas y el cumplimiento de normas de calidad e higiene.
+
+          
+
+
+
 
     separador
     #t_2_5.titulo-segundo.mb-5
